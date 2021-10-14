a876e08f2b236e60f750155aa3677f7fba57a655
borre <borre@haechi.io>
2021-10-14 16:38:57 +0900

    feat: add BSC recovery script

---
+++ example/get-bsc-account-address.ts
@@ -0,0 +1,15 @@
+import { BscRecovery } from "../src/bsc-recovery";
+import { Env } from "../src/recovery";
+
+function main() {
+    const recovery = new BscRecovery({
+        host: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+        accountKeyFile: '{"iv":"xQROYFmGOzK1pNB5hEjB6g==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"RWEyXMj0Mc0=","ct":"wIA/nF0kJ8zErj3Q7qrE0uGbFPyIEYPRRYL+K5swgc4oWcz2Oxtlw7d1/OlqRokTJAhvRFYJ342ui3qDSir+HbTMLsaVziECucw="}',
+        backupKeyFile: '{"iv":"kEmtkqX4talQDOhyWXJckA==","v":1,"iter":10000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"jBs385GnWp0=","ct":"UQMB5A2ixhZZpN04wZPV1PKkYyPOmmL1jdtnkcKjsih96upCUISVZF7gl0OW5j5ah6nJRk5I5VB0UwH/IXGniora733gSOvzCTs="}',
+        passphrase: "passphrase",
+        env: Env.Test
+    });
+    console.log(recovery.getAccountKeyAddress());
+}
+
+main();
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./btc-recovery";
 export * from "./eth-recovery";
 export * from "./klay-recovery";
 export * from "./ltc-recovery";
+export * from "./bsc-recovery";
